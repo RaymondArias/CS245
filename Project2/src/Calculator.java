99655745032bed8147ffc4124ac82b774ba774fd
RaymondArias <rarias2010@yahoo.com>
2015-10-25 12:14:34 -0700

    Implementing action listener

--- Project2/src/Calculator.java
+++ Project2/src/Calculator.java
@@ -3,8 +3,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class Calculator implements ActionListener{
+	
+	private String operator1;
+	private String operand;
+	
 	JLabel calcScreen;
 	String [] buttonIcons = {"7" , "8", "9", "/", "4", "5", "6", "x", "1", "2", "3", "-", "0", "C", "=", "+"};
+	
 	public Calculator()
 	{
 		JFrame frame = new JFrame("Raymond Arias' Calculator");
@@ -20,6 +25,7 @@ public class Calculator implements ActionListener{
 		for(int i = 0; i < buttonIcons.length; i++)
 		{
 			JButton calcButtons = new JButton(buttonIcons[i]);
+			calcButtons.addActionListener(this);
 			buttons.add(calcButtons);
 		}
 		
@@ -35,14 +41,63 @@ public class Calculator implements ActionListener{
 	
 	public void actionPerformed(ActionEvent ae) 
 	{
-		
+		//Button pushed was number
 		if(Character.isDigit(ae.getActionCommand().charAt(0)))
 		{
 			//Make method to get button push and add to label
+			if(calcScreen.getText().equals("0"))
+			{
+				calcScreen.setText(ae.getActionCommand());
+			}
+			else
+			{
+				calcScreen.setText(calcScreen.getText()+ ae.getActionCommand());
+			}
+		}
+		else if(isOperand(ae.getActionCommand()))
+		{
+			String operand = ae.getActionCommand();
+			if(operand.equals("+"))
+			{
+				operator1 = calcScreen.getText();
+				operand = ae.getActionCommand();
+			}
+			else if(operand.equals("-"))
+			{
+				operator1 = calcScreen.getText();
+				operand = ae.getActionCommand();
+			}
+			else if(operand.equals("x"))
+			{
+				operator1 = calcScreen.getText();
+				operand = ae.getActionCommand();
+			}
+			else
+			{
+				operator1 = calcScreen.getText();
+				operand = ae.getActionCommand();
+			}
+		}
+		else if(ae.getActionCommand().equals("="))
+		{
+			if(operand != null)
+			{
+				if(operand.equals("+"))
+				{
+					int newOutput = Integer.parseInt(operator1) + Integer.parseInt(calcScreen.getText());
+					calcScreen.setText(Integer.toString(newOutput));
+					operator1 = null;
+					operand = null;
+				}
+			}
 		}
 		
-		
-	}	
+	}
+	public boolean isOperand(String buttonPress)
+	{
+		return buttonPress.equals("+") || buttonPress.equals("-") 
+				|| buttonPress.equals("x") || buttonPress.equals("/");
+	}
 	public static void main(String []args)
 	{
 		SwingUtilities.invokeLater(new Runnable(){
