06ed95b8e2f7aa9df6cc1163f0d2c06aa153b16c
RaymondArias <rarias2010@yahoo.com>
2015-10-28 17:04:13 -0700

    Added comments to methods

--- Project2/src/Calculator.java
+++ Project2/src/Calculator.java
@@ -1,3 +1,11 @@
+//Name:	Arias, Raymond
+//Project: 2
+//Due: 10/30/15
+//Course: CS-245-01-f15
+//Description:
+//				An integer calculator that can do
+//				simple mathematical operations.
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,6 +21,10 @@ public class Calculator implements ActionListener{
 	JLabel calcScreen;
 	String [] buttonIcons = {"7" , "8", "9", "/", "4", "5", "6", "x", "1", "2", "3", "-", "0", "C", "=", "+"};
 	
+	/**
+	 * Constructor which creates frame and components used in 
+	 * frame, it also sets all initial values
+	 */
 	public Calculator()
 	{
 		operatorStack = new Stack<>();
@@ -30,7 +42,7 @@ public class Calculator implements ActionListener{
 			
 			e.printStackTrace();
 		}
-		frame.setSize(600, 200);
+		frame.setSize(400, 150);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		frame.setLayout(new GridLayout(2,1));
@@ -38,31 +50,37 @@ public class Calculator implements ActionListener{
 		JPanel display = new JPanel();
 		JPanel buttons = new JPanel();
 		buttons.setLayout(new GridLayout(4,4));
-		
+		display.setLayout(new BorderLayout());
 		
 		
 		for(int i = 0; i < buttonIcons.length; i++)
 		{
 			JButton calcButtons = new JButton(buttonIcons[i]);
 			calcButtons.addActionListener(this);
+			if(buttonIcons[i].equals("C"))
+				calcButtons.setMnemonic('C');
 			if(buttonIcons[i].equals("="))
 				frame.getRootPane().setDefaultButton(calcButtons);
 			buttons.add(calcButtons);
 		}
 		
-		calcScreen = new JLabel("0", JLabel.RIGHT);
-		calcScreen.setHorizontalAlignment(JLabel.RIGHT);
-		calcScreen.setVerticalAlignment(JLabel.CENTER);
-		
-		display.add(calcScreen);
+		calcScreen = new JLabel("0", FlowLayout.RIGHT);
+		Font font = new Font("SANS_SERIF", Font.ROMAN_BASELINE, 30);
+		calcScreen.setFont(font);
+		display.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+		display.add(calcScreen, BorderLayout.EAST);
 		display.setBackground(Color.WHITE);
 		frame.add(display);
 		frame.add(buttons);
 		
 		frame.setVisible(true);
 	}
-
 	
+
+	/**
+	 * Event Listener which handles all the event
+	 * the calculator programe uses
+	 */
 	public void actionPerformed(ActionEvent ae) 
 	{
 		//Program is in an error state
@@ -133,6 +151,8 @@ public class Calculator implements ActionListener{
 			if((ae.getModifiers() & ActionEvent.CTRL_MASK) != 0)
 			{
 				calcScreen.setText("(c) 2015 Raymond Arias");
+				inErrorState = true;
+				return;
 			}
 			else
 			{
@@ -143,11 +163,22 @@ public class Calculator implements ActionListener{
 			return;
 		
 	}
+	/**
+	 * Boolean method which checks to if see event was an operand button
+	 * @param buttonPress
+	 * @return
+	 */
 	public boolean isOperand(String buttonPress)
 	{
 		return buttonPress.equals("+") || buttonPress.equals("-") 
 				|| buttonPress.equals("x") || buttonPress.equals("/");
 	}
+	/**
+	 * Method used to do all mathematical operations
+	 * and returns values that may need to be push onto
+	 * operand stack
+	 * @return
+	 */
 	public Long doOperation()
 	{
 		Long newOutput;
@@ -155,7 +186,7 @@ public class Calculator implements ActionListener{
 		{
 			if(operandStack.peek().equals("+"))
 			{
-				newOutput = operatorStack.peek() + Integer.parseInt(calcScreen.getText());
+				newOutput = operatorStack.peek() + Long.parseLong(calcScreen.getText());
 				calcScreen.setText(Long.toString(newOutput));
 				operatorStack.pop();
 				operandStack.pop();
@@ -163,7 +194,7 @@ public class Calculator implements ActionListener{
 			}
 			else if(operandStack.peek().equals("-"))
 			{
-				newOutput = operatorStack.peek() - Integer.parseInt(calcScreen.getText());
+				newOutput = operatorStack.peek() - Long.parseLong(calcScreen.getText());
 				calcScreen.setText(Long.toString(newOutput));
 				operatorStack.pop();
 				operandStack.pop();
@@ -171,7 +202,7 @@ public class Calculator implements ActionListener{
 			}
 			else if(operandStack.peek().equals("x"))
 			{
-				newOutput = operatorStack.peek() * Integer.parseInt(calcScreen.getText());
+				newOutput = operatorStack.peek() * Long.parseLong(calcScreen.getText());
 				calcScreen.setText(Long.toString(newOutput));
 				operatorStack.pop();
 				operandStack.pop();
@@ -182,7 +213,7 @@ public class Calculator implements ActionListener{
 			{
 				if(isError(Integer.parseInt(calcScreen.getText())))
 					return null;
-				newOutput = operatorStack.peek() / Integer.parseInt(calcScreen.getText());
+				newOutput = operatorStack.peek() / Long.parseLong(calcScreen.getText());
 				calcScreen.setText(Long.toString(newOutput));
 				operatorStack.pop();
 				operandStack.pop();
@@ -193,6 +224,11 @@ public class Calculator implements ActionListener{
 		return null;
 		
 	}
+	/**
+	 * Pushes data from calculator screen onto operand stack
+	 * and operators onto operator stack
+	 * @param tempOperand
+	 */
 	public void pushOperation(String tempOperand)
 	{
 		if(inErrorState || isError(null))
@@ -220,6 +256,14 @@ public class Calculator implements ActionListener{
 			operandStack.push(tempOperand);
 		}
 	}
+	/**
+	 * Checks if calculator has any errors, either overflow
+	 * caused by going over the 10 digit limit or division by
+	 * zero. If either error occurs program outputs an error 
+	 * message and enters error state.
+	 * @param denominator
+	 * @return
+	 */
 	public boolean isError(Integer denominator)
 	{
 		
@@ -240,6 +284,10 @@ public class Calculator implements ActionListener{
 		}
 		return false;
 	}
+	/**
+	 * Clears the data in both stacks, set screen to "0",
+	 * and set isNewNumber flag to true
+	 */
 	public void clearOperation()
 	{
 		calcScreen.setText("0");
